522456d62b843d8223ae692d822af5ef22952caa
Lidia <joni.rea@hotmail.com>
2020-10-22 22:08:45 -0300

    Lo mejor en tecnologia

---
+++ src/componentes/Navbar/MenuItems.js
@@ -0,0 +1,24 @@
+export const MenuItems = [ 
+    {   
+        tittle: 'Celulares',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {   
+        tittle: 'Pcs',
+        url: '#',
+        cName: 'nav-links'
+    },{   
+        tittle: 'Accesorios',
+        url: '#',
+        cName: 'nav-links'
+    },{   
+        tittle: 'Perifericos',
+        url: '#',
+        cName: 'nav-links'
+    },{   
+        tittle: 'Nuestras redes',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
