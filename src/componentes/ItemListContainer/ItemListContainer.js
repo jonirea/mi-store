a75fd0b1208880140186f437bfe129a48994fc5c
Lidia <joni.rea@hotmail.com>
2020-11-02 23:51:51 -0300

    Lo mejor en tecnologia

--- src/componentes/ItemListContainer/ItemListContainer.js
+++ src/componentes/ItemListContainer/ItemListContainer.js
@@ -1,5 +1,30 @@
 import React from 'react';
 
+const datos = [{    
+    id: 1,
+    tittle: 'Xiaomi MI 10',
+    price: '$100000'
+},{ 
+    id: 2,
+    tittle: 'Samsung s10',
+    price: '$90000'
+}];
+
+const getDatos = () => {    
+    return new Promise((resolve, reject) => {    
+        setTimeout(() => {  
+            resolve(datos);
+        }, 2000);
+    });
+}
+
+getDatos().then((datos) => console.log(datos));
+
+
+
+
+
 const ItemListContainer =_ => <tittle className= "tittle"></tittle>
 
 export default ItemListContainer;
+
